b44f0d7cb1946b08ee81d40baf080b5d5e285942
Raphaël LABESTE <raphael.labeste@etudiant.univ-reims.fr>
2024-06-10 14:19:02 +0200

    Modifcation index.php pour avoir la structure de bas de la page

--- public/index.php
+++ public/index.php
@@ -2,4 +2,18 @@
 
 declare(strict_types=1);
 
-echo 'Hello Music!';
+use Html\WebPage;
+
+$titre = "Série TV";
+$webPage = new WebPage($titre);
+$webPage->appendContent("<h1>$titre</h1>");
+
+$webPage->appendContent("<a class='serie'>
+    <img src='default/default.png' alt='Poster'>
+    <a class='titre'>Titre série</a>
+    <a class='description'>Description série</a>
+</a>");
+
+
+
+echo $webPage->toHTML();
