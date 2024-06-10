d4412bd13e675eb389c7d14fd25af96f52c8e582
Valentin Durussel <valentin.durussel@etudiant.univ-reims.fr>
2024-06-10 10:38:15 +0200

    Création de la classe genre avec ses getters

---
+++ src/Entity/genre.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Entity;
+
+class genre
+{
+    private int $id;
+    private string $name;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
