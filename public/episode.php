d9b9db9ee3c9a07d725d0cecfa93a84c63be4a5d
Valentin Durussel <valentin.durussel@etudiant.univ-reims.fr>
2024-06-10 16:51:41 +0200

    Integration de la base de donnée dans episode.php

--- public/episode.php
+++ public/episode.php
@@ -1,29 +1,35 @@
 <?php
 
 declare(strict_types=1);
+
+use Entity\TVShow;
 use Html\WebPage;
 use Entity\Collection\CollectionEpisode;
+use Entity\Season;
 
 $webpage = new WebPage();
 $SeasonId = (int)$_GET['seasonId'];
 
-$titre = "Série TV: ";
+$titre = "Série TV: {}";
 
 $webPage = new WebPage($titre);
 $webPage->appendContent("<h1>$titre</h1>");
 
 $stmt = CollectionEpisode::findBySeasonId($SeasonId);
 
+$season = Season::findById($SeasonId);
+$serie = TVShow::findById($season->getTvShowId());
+
 $webpage->appendContent("<img src='default/default.png' alt='Poster'>
-    <a class='titre_serie'>{$_GET['name']}</a>
-    <a class='titre_saison'>Titre saison</a>
+    <a class='titre_serie' href='season.php?tvShowId={$season->getTvShowId()}'>{$serie->getName()}</a>
+    <a class='titre_saison'>{$season->getName()}</a>
 ");
 
 foreach ($stmt as $ligne) {
-    $webPage->appendContent("<a class='serie'>
-    <a class='num_ep'>Numéro episode 1</a>
-    <a class='titre_ep'>Titre episode 1</a>
-    <a class='description_ep'>Description episode 1</a>
+    $webPage->appendContent("<a class='episodes'>
+    <a class='num_ep'>{$webPage->escapeString((string)$ligne->getEpisodeNumber())}</a>
+    <a class='titre_ep'>{$webPage->escapeString((string)$ligne->getName())}</a>
+    <a class='description_ep'>{$webPage->escapeString((string)$ligne->getOverview())}</a>
 </a>");
 }
 
