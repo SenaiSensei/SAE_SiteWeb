17aac73da8163d9f630794a401c582de49d26a04
Valentin Durussel <valentin.durussel@etudiant.univ-reims.fr>
2024-06-10 11:32:35 +0200

    Création de la classe Episode avec ses getters

---
+++ src/Entity/Episode.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Entity;
+
+class Episode
+{
+    private int $id;
+    private int $seasonId;
+    private string $name;
+    private string $overview;
+    private int $episodeNumber;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getSeasonId(): int
+    {
+        return $this->seasonId;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getOverview(): string
+    {
+        return $this->overview;
+    }
+
+    public function getEpisodeNumber(): int
+    {
+        return $this->episodeNumber;
+    }
+
+
+}
