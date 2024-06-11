dbdc5c6eecd63bf6ce53d79a703fc5331aa69b83
Valentin Durussel <valentin.durussel@etudiant.univ-reims.fr>
2024-06-11 15:55:49 +0200

    Modification

--- public/season.php
+++ public/season.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use Entity\Collection\CollectionSeason;
-use Entity\Season;
 use Entity\TVShow;
 use Html\AppWebPage;
 
@@ -13,20 +12,19 @@ $serie = TVShow::findById($TVShowId);
 $title = "Série TV: {$serie->getName()}";
 
 $webPage = new AppWebPage($title);
-$webPage->appendContent("<h1>$title</h1>");
 
 
 $webPage->appendContent("<img src='default/default.png' alt='Poster'>
     <a class='title_serie'>{$serie->getName()}</a>
     <a class='origin_title_serie'>{$serie->getOriginalName()}</a>
-    <a class='origin_title_serie'>{$serie->getOverview()}</a>
+    <a class='desc_title_serie'>{$serie->getOverview()}</a>
 ");
 
 $stmt = CollectionSeason::findByTVShowId($TVShowId);
 foreach ($stmt as $ligne) {
     $webPage->appendContent("<section class='season'>
     <img src='default/default.png' alt='Poster'>
-    <a class='season_title'>{$ligne->getName()}</a>
+    <a class='season_title' href='episode.php?seasonId={$ligne->getId()}'>{$ligne->getId()}:id {$ligne->getTVShowId()}:TvShowId /{$ligne->getName()}</a>
 </section>");
 }
 
