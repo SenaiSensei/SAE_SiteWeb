a03a3904c682e2577b61a5bb3a40f2183fb2930a
Valentin Durussel <valentin.durussel@etudiant.univ-reims.fr>
2024-06-11 16:30:47 +0200

    Intégration de la BDD  et intégration des posters

--- public/season.php
+++ public/season.php
@@ -12,18 +12,19 @@ $serie = TVShow::findById($TVShowId);
 $title = "Série TV: {$serie->getName()}";
 
 $webPage = new AppWebPage($title);
+$webPage->appendMenu("<a href='index.php?genre=0' class='menu_accueil'>Accueil</a>");
 
 
-$webPage->appendContent("<img src='default/default.png' alt='Poster'>
+$webPage->appendContent("<img src='poster.php?posterId={$serie->getPosterId()}' alt='Poster'>
     <a class='title_serie'>{$serie->getName()}</a>
     <a class='origin_title_serie'>{$serie->getOriginalName()}</a>
     <a class='desc_title_serie'>{$serie->getOverview()}</a>
 ");
 
-$stmt = CollectionSeason::findByTVShowId($TVShowId);
+$stmt = CollectionSeason::findByTVShowId((int)$_GET['tvShowId']);
 foreach ($stmt as $ligne) {
     $webPage->appendContent("<section class='season'>
-    <img src='default/default.png' alt='Poster'>
+    <img src='poster.php?posterId={$ligne->getPosterId()}' alt='Poster'>
     <a class='season_title' href='episode.php?seasonId={$ligne->getId()}'>{$ligne->getId()}:id {$ligne->getTVShowId()}:TvShowId /{$ligne->getName()}</a>
 </section>");
 }
