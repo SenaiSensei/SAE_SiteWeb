897625f80af4416bd02e6b5ec6b58a3be9688d0a
Raphaël LABESTE <raphael.labeste@etudiant.univ-reims.fr>
2024-06-11 13:53:30 +0200

    Changeent Menu episode.php

--- public/episode.php
+++ public/episode.php
@@ -36,7 +36,7 @@ $titre = "Série TV: {$serie->getName()}";
 $titreSeason = $season->getName();
 
 $webPage = new AppWebPage($titre." ".$titreSeason);
-$webPage->appendToHead("<section class='menu'><a href='index.php' class='menu_accueil'>Accueil</a>
+$webPage->appendMenu("<section class='menu'><a href='index.php' class='menu_accueil'>Accueil</a>
 </section>");
 $webPage->appendCssUrl("css/episode.css");
 $webPage->appendToHead("<meta name='description' content='Author: R.L., An app web to view and modify saves TV shows on a database'>");
