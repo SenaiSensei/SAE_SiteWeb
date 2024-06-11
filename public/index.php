c006e29576a39d34b61e529b3f777d5f0e831b65
Raphaël LABESTE <raphael.labeste@etudiant.univ-reims.fr>
2024-06-11 15:42:09 +0200

    Insertion filtre dans la page web de index.php

--- public/index.php
+++ public/index.php
@@ -3,28 +3,54 @@
 declare(strict_types=1);
 
 use Entity\Collection\CollectionGenre;
+use Entity\Exception\EntityNotFoundException;
 use Html\AppWebPage;
 use Entity\Collection\CollectionTVShow;
-use Entity\Genre;
+
+if (!isset($_GET['genre'])) {
+    header('Location: http://localhost:8000/index.php?genre=0');
+}
 
 $titre = "Série TV";
 $webPage = new AppWebPage($titre);
 $webPage->appendCssUrl("css/index.css");
 $webPage->appendToHead("<meta name='description' content='Author: R.L., An app web to view and modify saves TV shows on a database'>");
 
-$webPage->appendMenu("<a href='index.php' class='menu_accueil'>Accueil</a>");
-
-
+$webPage->appendMenu("<a href='index.php?genre=0' class='menu_accueil'>Accueil</a>");
+$webPage->appendFiltre("<form action='index.php' method='get' class='filter'>Filtre : <label> <label><input type='radio' name='genre' value='0'>Aucun</label></label>");
 $genres = CollectionGenre::findAll();
 
-$stmt = CollectionTVShow::findAll();
+foreach ($genres as $ligne) {
+    $webPage->appendFiltre("<label><input type='radio' name='genre' value='{$ligne->getId()}'>{$ligne->getName()}</label>");
+}
 
-foreach ($stmt as $ligne) {
-    $webPage->appendContent("<a class='serie' href='season.php/TVShowId={$webPage->escapeString((string)$ligne->getId())}'>
-    <img src='poster.php?posterId={$ligne->getPosterId()}' alt='Poster'>
-    <section> <section class='titre'>{$webPage->escapeString((string)$ligne->getName())}</section>
-    <section class='description'>{$webPage->escapeString((string)$ligne->getOverview())}</section></section>
-</a>");
+
+$webPage->appendFiltre("<button type='submit'>Search</button></label></form>");
+
+if (!ctype_digit($_GET['genre'])) {
+    header('Location: http://localhost:8000/index.php?genre=0');
+}
+
+if ($_GET['genre'] == 0) {
+    $stmt = CollectionTVShow::findAll();
+
+    foreach ($stmt as $ligne) {
+        $webPage->appendContent("<a class='serie' href='season.php/TVShowId={$webPage->escapeString((string)$ligne->getId())}'>
+        <img src='poster.php?posterId={$ligne->getPosterId()}' alt='Poster'>
+        <section> <section class='titre'>{$webPage->escapeString((string)$ligne->getName())}</section>
+        <section class='description'>{$webPage->escapeString((string)$ligne->getOverview())}</section></section>
+    </a>");
+    }
+} else {
+    $stmt = CollectionTVShow::findByGenreId((int)$_GET['genre']);
+
+    foreach ($stmt as $ligne) {
+        $webPage->appendContent("<a class='serie' href='season.php/TVShowId={$webPage->escapeString((string)$ligne->getId())}'>
+        <img src='poster.php?posterId={$ligne->getPosterId()}' alt='Poster'>
+        <section> <section class='titre'>{$webPage->escapeString((string)$ligne->getName())}</section>
+        <section class='description'>{$webPage->escapeString((string)$ligne->getOverview())}</section></section>
+        </a>");
+    }
 }
 
 
