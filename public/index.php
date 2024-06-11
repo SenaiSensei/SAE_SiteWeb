f9b91aba34731e2ae882216e3ec6b37f67da7728
Raphaël LABESTE <raphael.labeste@etudiant.univ-reims.fr>
2024-06-11 10:13:35 +0200

    Modification  HTML pour convenir à l'UI attendu.

--- public/index.php
+++ public/index.php
@@ -14,8 +14,8 @@ $stmt = CollectionTVShow::findAll();
 foreach ($stmt as $ligne) {
     $webPage->appendContent("<a class='serie' href='season.php/seasonId={$webPage->escapeString((string)$ligne->getId())}'>
     <img src='poster.php?posterId={$ligne->getPosterId()}' alt='Poster'>
-    <section class='titre'>{$webPage->escapeString((string)$ligne->getName())}</section>
-    <section class='description'>{$webPage->escapeString((string)$ligne->getOverview())}</section>
+    <section> <section class='titre'>{$webPage->escapeString((string)$ligne->getName())}</section>
+    <section class='description'>{$webPage->escapeString((string)$ligne->getOverview())}</section></section>
 </a>");
 }
 
