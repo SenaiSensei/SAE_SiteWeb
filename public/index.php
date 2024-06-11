3ce8b70ee504bbc80e8e0a757aa26224aade795e
Raphaël LABESTE <raphael.labeste@etudiant.univ-reims.fr>
2024-06-11 13:36:10 +0200

    Modification index.php pour ajouter le filtre

--- public/index.php
+++ public/index.php
@@ -2,8 +2,10 @@
 
 declare(strict_types=1);
 
+use Entity\Collection\CollectionGenre;
 use Html\AppWebPage;
 use Entity\Collection\CollectionTVShow;
+use Entity\Genre;
 
 $titre = "Série TV";
 $webPage = new AppWebPage($titre);
@@ -11,15 +13,23 @@ $webPage->appendCssUrl("css/index.css");
 $webPage->appendToHead("<meta name='description' content='Author: R.L., An app web to view and modify saves TV shows on a database'>");
 
 $webPage->appendToHead("<section class='menu'><a href='index.php' class='menu_accueil'>Accueil</a>
-<form action='filtre.php' method='GET' class='filtre'>
+<form action='index.php' method='POST' class='filtre'>
 <p>Filtre</p>
+<label><input name='id' type='radio' value='0' checked>Aucun</label>
 <section class='value_filtre'>");
 
-$stmt = CollectionTVShow::findAll();
 
-foreach ($stmt as $ligne) {
+
+$genres = CollectionGenre::findAll();
+
+foreach ($genres as $ligne) {
     $webPage->appendToHead("
-    <label><input name='id' type='radio' value='1'>1</label>");
+    <label><input name='id' type='radio' value='{$ligne->getId()}'>{$ligne->getName()}</label>");
+}
+
+
+if ($_POST[''] == 0) {
+    $stmt = CollectionTVShow::findAll();
 }
 
 $webPage->appendToHead("</section></form> </section>");
