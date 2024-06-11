6f119d937a3d1272de430fc17ea2286ee3673528
Raphaël LABESTE <raphael.labeste@etudiant.univ-reims.fr>
2024-06-11 13:57:10 +0200

    Modification lien vers season.php

--- public/index.php
+++ public/index.php
@@ -20,7 +20,7 @@ $genres = CollectionGenre::findAll();
 $stmt = CollectionTVShow::findAll();
 
 foreach ($stmt as $ligne) {
-    $webPage->appendContent("<a class='serie' href='season.php/seasonId={$webPage->escapeString((string)$ligne->getId())}'>
+    $webPage->appendContent("<a class='serie' href='season.php/TVShowId={$webPage->escapeString((string)$ligne->getId())}'>
     <img src='poster.php?posterId={$ligne->getPosterId()}' alt='Poster'>
     <section> <section class='titre'>{$webPage->escapeString((string)$ligne->getName())}</section>
     <section class='description'>{$webPage->escapeString((string)$ligne->getOverview())}</section></section>
