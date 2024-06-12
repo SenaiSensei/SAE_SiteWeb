189c326c839265bc8236fa34f6b601b67b68ce06
Raphaël LABESTE <raphael.labeste@etudiant.univ-reims.fr>
2024-06-12 09:50:41 +0200

    création de la fonction getHtmlForm() dans TVShowForm.php permettant la création d'un formulaire pour créer / insérer un TVShow.

--- src/Html/Form/TVShowForm.php
+++ src/Html/Form/TVShowForm.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 namespace Html\Form;
 
 use Entity\TVShow;
+use Html\StringEscaper;
 
 class TVShowForm
 {
+    use StringEscaper;
+
     private ?TVShow $tvShow;
 
     /**
@@ -22,4 +25,40 @@ class TVShowForm
         return $this->tvShow;
     }
 
+    public function getHtmlForm(string $action): string
+    {
+        $form = <<<HTML
+            <form action="$action" name="" method="post"></form>
+            <label><input name="id" type="hidden" value="{$this?->getTvShow()?->getId()}"></label>
+        HTML;
+        if ($this->escapeString($this?->getTvShow()?->getName()) != null) {
+            $form .= <<<HTML
+            <label class="Nom"><input name="name" type="text" value="{$this->escapeString($this?->getTvShow()?->getName())}" required></label>
+        HTML;
+        }
+
+        if ($this->escapeString($this?->getTvShow()?->getOriginalName()) != null) {
+            $form .= <<<HTML
+            <label class="NomOriginal"><input name="originalName" type="text" value="{$this->escapeString($this?->getTvShow()?->getOriginalName())}" required></label>
+        HTML;
+        }
+
+        if ($this->escapeString($this?->getTvShow()?->getHomePage()) != null) {
+            $form .= <<<HTML
+            <label class="homePage"><input name="homePage" type="text" value="{$this->escapeString($this?->getTvShow()?->getHomePage())}"></label>
+        HTML;
+        }
+
+        if ($this->escapeString($this?->getTvShow()?->getOverview()) != null) {
+            $form .= <<<HTML
+            <label class="Overview"><input name="overview" type="text" value="{$this->escapeString($this?->getTvShow()?->getOverview())}" required></label>
+        HTML;
+        }
+        $form .= <<<HTML
+            <button type="submit">Save</button
+            </form>
+        HTML;
+
+        return $form;
+    }
 }
