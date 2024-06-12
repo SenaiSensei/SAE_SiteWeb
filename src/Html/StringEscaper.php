8d4f1a221912330c1f18ce4746f2b36763e7efb8
Raphaël LABESTE <raphael.labeste@etudiant.univ-reims.fr>
2024-06-12 09:40:56 +0200

    Création du trait StringEscaper.php pour protéger les donées des chaînes de caractères.

---
+++ src/Html/StringEscaper.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace Html;
+
+trait StringEscaper
+{
+    /**
+     * Protéger les caractères spéciaux pouvant dégrader la page Web.
+     *
+     * @param ?string $string La chaîne à protéger
+     * @return string La chaîne protégée
+     */
+    public function escapeString(?string $text): string
+    {
+        $chaine = htmlspecialchars($text, ENT_QUOTES | ENT_HTML5 | ENT_SUBSTITUTE);
+
+        return $chaine;
+    }
+}
